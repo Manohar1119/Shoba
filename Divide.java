85aea71f499d04f044c483a011bf486fb2f72519
Manohar1119 <manohar.pasem@gmail.com>
2022-09-22 12:05:36 +0530

    manu

--- Divide.java
+++ Divide.java
@@ -5,8 +5,9 @@ public class Divide {
 		System.out.println("Welcome");
 		int num=10;
 		int num1=0;
+		int num3=100;
 		try {
-			int num2=num/num1;
+			int num2=num*num3/num1;
 			System.out.println(num2);
 		}catch(ArithmeticException e) {
 			e.printStackTrace();
